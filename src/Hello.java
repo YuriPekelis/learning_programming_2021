3723d26be5981ded96737bfd028e7045ecc71ee8
Yurii Pekelis <pekelis@gmail.com>
2021-06-06 01:04:17 +0300

    Hello + Dog_start

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        String tr = new String("vfjhsbcsjda");
+        String tr1 = "fbgfddfbgdf";
+        Dog jack = new Dog();
+        jack.sayName();
+        for(int i=0; i< 20; i++) {
+            String name = "Ben";
+            System.out.println("Hello " + name);
+        }
+        if (3 >2) {
+            System.out.println("3>2");
+        }
+
+    }
+}
