f54b6cf5565fbdc50202e342950ac12659b45951
Yurii Pekelis <pekelis@gmail.com>
2021-07-04 11:23:11 +0300

    added comments method code, remove redundant line

--- src/yurii/coder/Coder.java
+++ src/yurii/coder/Coder.java
@@ -6,15 +6,14 @@ public class Coder {
     private static final int startAscii = 97;
     private static final int finalAscii = 122;
     public static String code (String word,String codeWord) throws IllegalArgumentException {
-        int abcLength = finalAscii - startAscii + 1;
         String codedWord = "";
         for(int i=0; i<word.length(); i++){
             if ((word.charAt(i)<startAscii)||(word.charAt(i)>finalAscii)){
                 throw new IllegalArgumentException(String.format("Symbol '%s' is illegal", word.charAt(i)));
             }
-            int wordSymbolInABC = word.charAt(i) - startAscii + 1;
-            int codeWordSymbolInABC = codeWord.charAt(i % codeWord.length()) - startAscii + 1;
-            int codedSymbolInABC = (wordSymbolInABC + codeWordSymbolInABC) % 26;
+            int wordSymbolInABC = word.charAt(i) - startAscii + 1; // number of the current symbol of word in alphabet
+            int codeWordSymbolInABC = codeWord.charAt(i % codeWord.length()) - startAscii + 1; // number of the current symbol of codeWord in alphabet
+            int codedSymbolInABC = (wordSymbolInABC + codeWordSymbolInABC) % 26; // number of the codded symbol in alphabet
             codedWord = codedWord + (char)(codedSymbolInABC + startAscii -1);
         }
         return codedWord;
