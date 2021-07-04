a5bf718238198cda0ce4cb913caadc9a7d9bd24c
Yurii Pekelis <pekelis@gmail.com>
2021-07-04 11:18:58 +0300

    finished code method

--- src/yurii/coder/Coder.java
+++ src/yurii/coder/Coder.java
@@ -1,10 +1,25 @@
 package yurii.coder;
 
-public class Coder {
-    public static String code (String word,String codeWord){
+import java.util.prefs.BackingStoreException;
 
+public class Coder {
+    private static final int startAscii = 97;
+    private static final int finalAscii = 122;
+    public static String code (String word,String codeWord) throws IllegalArgumentException {
+        int abcLength = finalAscii - startAscii + 1;
+        String codedWord = "";
+        for(int i=0; i<word.length(); i++){
+            if ((word.charAt(i)<startAscii)||(word.charAt(i)>finalAscii)){
+                throw new IllegalArgumentException(String.format("Symbol '%s' is illegal", word.charAt(i)));
+            }
+            int wordSymbolInABC = word.charAt(i) - startAscii + 1;
+            int codeWordSymbolInABC = codeWord.charAt(i % codeWord.length()) - startAscii + 1;
+            int codedSymbolInABC = (wordSymbolInABC + codeWordSymbolInABC) % 26;
+            codedWord = codedWord + (char)(codedSymbolInABC + startAscii -1);
+        }
+        return codedWord;
     }
     public static String decode (String codedWord, String decoderWord){
-
+        return "";
     }
 }
