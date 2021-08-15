334211690e9694e9de7a5799540fb55e48bb0f46
andriykhomik <andriykhomik1992@gmail.com>
2021-08-15 10:33:20 +0300

    Created addBook and added gitIgnore

--- src/andriy/library/LibraryOperator.java
+++ src/andriy/library/LibraryOperator.java
@@ -1,15 +1,53 @@
 package andriy.library;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class LibraryOperator {
 
     private String filepath;
+    private String[] headers;
+    private File libraryFile;
 
-    public LibraryOperator(String filepath) {
+    public LibraryOperator(String filepath) throws FileNotFoundException {
         this.filepath = filepath;
+        this.readHeaders();
+    }
+
+    private void readHeaders () throws FileNotFoundException {
+        try {
+            this.libraryFile = new File(this.filepath);
+            Scanner myReader = new Scanner(this.libraryFile);
+            this.headers = myReader.nextLine().split(";");
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            throw e;
+        }
     }
 
     public void addBook() {
-
+        List <String> currentBook = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        for (int i=0; i<headers.length; i++) {
+            System.out.println("Please add your book " + headers[i]);
+            currentBook.add(scanner.nextLine());
+        }
+        try {
+            FileWriter libraryFile = new FileWriter(this.filepath);
+            String resultString = String.join(";", currentBook);
+            libraryFile.write(resultString);
+            libraryFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void deleteBook() {
--- src/andriy/library/LibraryRunner.java
+++ src/andriy/library/LibraryRunner.java
@@ -1,11 +1,14 @@
 package andriy.library;
 
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class LibraryRunner {
-    public static void main(String[] args) {
-        LibraryOperator library = new LibraryOperator("library.csv");
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(Paths.get("").toAbsolutePath().toString());
+        LibraryOperator library = new LibraryOperator("src/andriy/library/library.csv");
         while (true) {
             System.out.println("1. Add book\n2. Delete book\n3. Search book\n4. Exit");
             System.out.println("Input operation:");
@@ -16,7 +19,7 @@ public class LibraryRunner {
             } catch (InputMismatchException e) { }
 
             if(operation == 1){
-
+                library.addBook();
             } else if(operation == 2) {
 
             } else if (operation == 3){
